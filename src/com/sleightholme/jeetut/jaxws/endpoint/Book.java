63935fed64672cd6abeed663d2ba0f98161dc1e9
Cousjava <jonathan.coustick@payara.fish>
2017-05-30 13:19:43 +0100

    Added JAX-RS endpoints

--- src/com/sleightholme/jeetut/jaxws/endpoint/Book.java
+++ src/com/sleightholme/jeetut/jaxws/endpoint/Book.java
@@ -5,8 +5,13 @@ import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebParam.Mode;
 import javax.jws.WebService;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
 
+@Path("/book")
 @WebService
 public class Book {
 
@@ -24,18 +29,21 @@ public class Book {
 		this.ISBN = ISBN;
 	}
 	
+        @GET
 	public String getTitle(){
 		return title;
 	}
 	
+        @POST
 	@WebMethod
-	public Response setTitle(@WebParam(name="title", mode = Mode.IN) String title){
+	public Response setTitle(@PathParam("title") @WebParam(name="title", mode = Mode.IN) String title){
 		this.title = title;
 		return Response.status(200).build();
 	}
 	
+        @GET
 	@WebMethod
-	public String sayHello(String name){
+	public String sayHello(@PathParam("name") String name){
 		return "Hello " + name + "!";
 	}
 	
